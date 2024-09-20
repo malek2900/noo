6c60b207d19bb75d6db3ad8064ad356f71cd61e9
malek2900 <eng.malek200@gmail.com>
2024-09-20 10:48:36 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>الله اعلم</title>
+</head>
+<body>
+
+
+<?php
+
+for($i =1; $i<= 5050 ; $i++){
+    echo '<div class="card">';
+}
+
+
+
+?>
+
+
+    <img src="https://i0.wp.com/picjumbo.com/wp-content/uploads/lava-in-iceland-close-up-free-photo.jpeg?w=1024&quality=50" alt="">
+  <h2 class="card-title">السلام عيكم</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium itaque amet a perferendis! Ad, ipsa?</p>
+</div>
+</body>
+</html>
